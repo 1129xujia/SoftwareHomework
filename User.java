bf8ddcca3e7bf06482f76c0ee415f573d3780d59
1129xujia <1975014403@qq.com>
2023-12-09 10:07:37 +0800

    first

---
+++ User.java
@@ -0,0 +1,54 @@
+// User.java
+public class User {
+    private String u_id;
+    private String u_name;
+    private String u_code;
+    private String u_phone;
+    private String u_address;
+    private double u_money;
+    private String u_state;
+
+    public User(String u_id, String u_name, String u_code, String u_phone, String u_address) {
+        // constructor implementation
+    }
+
+    public void userLogin(String u_name, String u_code) {
+  
+        // user login implementation
+    }
+
+    public void userRegister(String u_name, String u_code) {
+        // user registration implementation
+    }
+
+    public boolean searchingGood(String o_name) {
+        return true;
+        // searching for goods implementation
+    }
+
+    public void checkInfo(User somebody) {
+        // check user information implementation
+    }
+
+    public void changeInfo(User somebody) {
+        // change user information implementation
+    }
+
+    public void changeName(String u_name) {
+        // change user name implementation
+    }
+
+    public void changePhone(String u_phone) {
+        // change user phone implementation
+    }
+
+    public void changeAddress(String u_address) {
+        // change user address implementation
+    }
+
+    public void recharge(double u_money) {
+        // recharge implementation
+    }
+}
+
+// Sellers.java
